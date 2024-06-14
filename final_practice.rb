438c50a6a97c837a3ab0cfe2a2e6846a3b9804b2
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-06-14 15:45:43 -0600

    Include answers to final_practice.rb and self_eval.md

--- final_practice.rb
+++ final_practice.rb
@@ -17,34 +17,88 @@ puts last_laugh
 
 # 1: Write a method named "greeting" that returns a string with a generic greeting. 
 
+def greeting
+  "Well hello there."
+end
+print greeting
+
 # What is the return value of your method?
+# The return value of greeting is "Well hello there" because `print greeting` was the last line of code (implicit)
 # How many arguments did you pass your method?
+# I belive I passed zero arguments because greeting has no defined parameters.
 
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
 
+
+custom_name = "Big Red"
+
+def custom_greeting(custom_name)
+ "Good afternoon, #{custom_name}, looking good!"
+end
+
+print custom_greeting(custom_name)
+
 # What is the return value of your method?
+# The return value of custom_greeting() is `print custom_greeting(custom_name)`
 # How many arguments did you pass your method?
+# The custom_greeting() method was passed one argument bacause the method only has 1 parameter.
 # What data type was your argument(s)?
+# The datatype was a string object.
 
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
 
+first_name = "Billy"
+middle_name = "Bob"
+last_name = "Thornton"
+
+def greet_person(first_name, middle_name, last_name)
+ "Greetings #{first_name} #{middle_name} #{last_name}, nice to meet you!"
+end
+
+print greet_person(first_name, middle_name, last_name)
+
+# What is the return value of your method?
+# The return value of the greet_person() method is `print greet_person(first_name, middle_name, last_name)`
+# How many arguments did you pass your method?
+# The greet_person() method has 3 parameters and takes 3 arguments.
+# What data type was your argument(s)?
+# All 3 arguments were the string object datatype.
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
+int = 100
+def square(int)
+  int * int
+end
+print square(int)
+
 # Bonus: Print a sentence that interpolates the return value of your square method. You should not have to update the method itself to do this.
+print "The square of #{int} is #{square(int)}."
 
 # What is the return value of your method?
+# After my Bonus code, the return value is `print "The square of #{int} is #{square(int)}."`
 # How many arguments did you pass your method?
+# In the square() method I passed in a single argument since the square() method has 1 parameter.
 # What data type was your argument(s)?
+# The square() method accepts interger data types as arguments.
 
 
 # 5: Write a method named "check_stock" that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+
+def check_stock(quantity, item)
+  if quantity >= 4
+    print "#{item} is stocked"
+  elsif quantity <= 0
+    print "#{item} - OUT of stock"
+  else
+    print "#{item} - running low"
+  end
+end
+
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
@@ -57,3 +111,5 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+# Why do these prompts end with a `;` like in javascript?
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,8 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+    - I'm proud of how much I've retained from the lectures this week. 
+    - When I compare my current self completing the CFUs vs my past self (like when I tried a few Try Turing! classes and felt completely lost) and see how far I've come it feels great!
+    - Even if I don't initially know how to complete an exercise, I can at least start it and reliably get 90% of the way in completing it.
 - What is the power of using methods in our applications?
+    - Methods can complete almost any task and once they're written they can be re-used indefinitely. This saves tons of time and energy we can devote to making even more powerful tools/applications.
